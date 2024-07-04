16e7d974f18762adfd5de8be35397fac973507e4
อาทิตย์ ขาวเอี่ยม <atit@sMacBookPro.local>
2024-07-04 16:18:53 +0700

    up

---
+++ src/app/MetaTags.tsx
@@ -0,0 +1,50 @@
+// components/MetaTags.tsx
+import Head from 'next/head';
+import type { Metadata } from "next";
+
+// const MetaTags: any = () => {
+//     const metadata: Metadata = {
+//         title: 'Hello Welcome to ... , My name Atit',
+//         description: 'Welcome to my professional resume page!!',
+//         openGraph: {
+//             url: "https://atit.site",
+//             type: "website",
+//             title: "Hello Welcome to ... , My name Atit",
+//             description: "Welcome to my professional resume page!!",
+//             images: [
+//                 {
+//                     url: "https://atit.site/homepage.png",
+//                     width: "1920",
+//                     height: "896",
+//                     alt: "Hello Welcome to ... , My name Atit"
+//                 }
+//             ]
+//         }
+
+//     };
+//     return (
+//         <Head>
+//             <title>{metadata.title}</title>
+//             <meta name="description" content={metadata.description} />
+
+//             {/* Open Graph Meta Tags */}
+//             <meta property="og:url" content={metadata.ogUrl} />
+//             <meta property="og:type" content={metadata.ogType} />
+//             <meta property="og:title" content={metadata.ogTitle} />
+//             <meta property="og:description" content={metadata.ogDescription} />
+//             <meta property="og:image" content={metadata.ogImage} />
+//             <meta property="og:image:width" content={metadata.ogImageWidth} />
+//             <meta property="og:image:height" content={metadata.ogImageHeight} />
+
+//             {/* Twitter Meta Tags */}
+//             <meta name="twitter:card" content={metadata.twitterCard} />
+//             <meta property="twitter:domain" content={metadata.twitterDomain} />
+//             <meta property="twitter:url" content={metadata.twitterUrl} />
+//             <meta name="twitter:title" content={metadata.twitterTitle} />
+//             <meta name="twitter:description" content={metadata.twitterDescription} />
+//             <meta name="twitter:image" content={metadata.twitterImage} />
+//         </Head>
+//     );
+// };
+
+// export default MetaTags;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,17 @@ export const metadata: Metadata = {
   description: "Welcome to my professional resume page!",
   openGraph: {
     url: "https://atit.site",
-    type: "profile",
+    type: "website",
     title: "Hello Welcome to ... , My name Atit",
     description: "Welcome to my professional resume page!!",
-    images: "https://atit.site/homepage.png"
+    images: [
+      {
+        url: "https://atit.site/homepage.png",
+        width: 1200,
+        height: 630,
+        alt: "Hello Welcome to ... , My name Atit"
+      }
+    ]
     // determiner: 'the',
     // title: "AK-DEV",
     // description: "Welcome to my professional resume page!",
@@ -39,11 +46,8 @@ export const metadata: Metadata = {
     // ttl: 3600
   },
 
-  //   <meta property="og:url"                content="https://contentmastery.io/on-page-seo/" />
-  // <meta property="og:type"               content="article" />
-  // <meta property="og:title"              content="On-Page SEO คืออะไร พร้อมแนวทางปฏิบัติที่ดีที่สุดเพื่อติดหน้าแรก Google" />
-  // <meta property="og:description"        content="On-Page SEO คือการปรับแต่งหน้าเว็บของเราให้ optimize หรือดีต่อ Google Search เป็นสิ่งที่เราสามารถควบคุมได้โดยตรง โดยส่วนใหญ่แล้วจะเกี่ยวข้องกับการเขียนและปรับคอนเทนต์บนหน้าเว็บไซต์ " />
-  // <meta property="og:image"  
+
+
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
